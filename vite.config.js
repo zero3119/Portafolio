75a9ef67f09f425c815214c0ad5e5f159e350339
zero3119 <127268380+zero3119@users.noreply.github.com>
2024-09-01 21:50:11 -0600

    change name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/zero3119.github.io/",
+  base: "/portafolio/",
 })
